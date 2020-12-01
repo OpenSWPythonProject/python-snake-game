d1e3e768f89cf79d8f8725aa59f7a23aa4031db9
BHyeonKim <rlaqhguse@gmail.com>
2020-12-02 00:30:36 +0900

    볼륨조절

--- music.py
+++ music.py
@@ -1,7 +1,5 @@
 import pygame
 
-from config import Config, screen
-
 
 class Music:
     def __init__(self):
@@ -11,16 +9,13 @@ class Music:
         pygame.mixer.music.stop()
         pygame.mixer.music.unload()
         pygame.mixer.music.load('res/themeMusic.mp3')
+        pygame.mixer.music.set_volume(0.3)
         pygame.mixer.music.play(-1)
 
     def playInGameMusic(self):
         pygame.mixer.music.stop()
         pygame.mixer.music.unload()
         pygame.mixer.music.load('res/inGameMusic.mp3')
+        pygame.mixer.music.set_volume(0.3)
         pygame.mixer.music.play(-1)
 
-    def drawText(self):
-        font = pygame.font.Font(None, 25)
-        text = font.render("Music off", True. Config.GREEN)
-        text_rect = text.get_rect(center=(Config.WINDOW_WIDTH/2, Config.WINDOW_HEIGHT/2 ))
-        screen.blit(text, text_rect)
