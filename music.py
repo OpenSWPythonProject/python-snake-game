d2caa35ff0fe096b470cdc4edd8e969c2d07f9d9
Seungsoo Han <h2sorginal@gmail.com>
2020-12-02 03:53:43 +0900

    볼륨조절

--- music.py
+++ music.py
@@ -9,13 +9,13 @@ class Music:
         pygame.mixer.music.stop()
         pygame.mixer.music.unload()
         pygame.mixer.music.load('res/themeMusic.mp3')
-        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.set_volume(0.02)
         pygame.mixer.music.play(-1)
 
     def playInGameMusic(self):
         pygame.mixer.music.stop()
         pygame.mixer.music.unload()
         pygame.mixer.music.load('res/inGameMusic.mp3')
-        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.set_volume(0.02)
         pygame.mixer.music.play(-1)
 
